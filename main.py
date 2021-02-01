68eddd480729c794b78bafc1e1e0f96f46e1572a
Conqave <74113069+Conqave@users.noreply.github.com>
2021-02-01 23:20:32 +0100

    Update main.py
    dodany kod komunikacji

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import mysql.connector
+import socket
+import time
+
+listensocket = socket.socket()
+Port = 8000
+maxConnections = 999
+IP = socket.gethostname()
+
+listensocket.bind(('', Port))
+
+listensocket.listen(maxConnections)
+print("Server has been started at " + IP + " on port " + str(Port))
+
+(clientsocket, address) = listensocket.accept()
+print("New connection has been made!")
+
+running = True
+
+while running:
+    message = clientsocket.recv(1024).decode()
+    print(message)
+    if not message == "":
+        time.sleep(5)
+
 from mysql.connector import Error
 
 try:
